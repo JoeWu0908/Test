270df47fbb544f4c91bba59db6b2503b7efd5b1f
joe.wu <jw840908@gmail.com>
2020-01-08 18:07:52 +0800

    'api-test1'

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ from flask import Flask, jsonify, request
 import pickle
 
 # load model
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('./model.pkl', 'rb'))
 
 # app
 app = Flask(__name__)
 
 # routes
-@app.route('/', methods=['POST'])
+
+@app.route('/')
+
+def hello():
+    return "MODEL TEST"
+
+
+@app.route('/model', methods=['POST'])
 
 def predict():
     # get data
